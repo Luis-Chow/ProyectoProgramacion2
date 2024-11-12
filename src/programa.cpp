79a08230c22810d720524446b8ddcea5db3ae4c3
Jesus Quintero <jaqg1106@gmail.com>
2024-11-12 17:43:40 -0400

    Feat: added update function

--- src/programa.cpp
+++ src/programa.cpp
@@ -58,17 +58,64 @@ void borrarFilaCSV(const string& archivo, int filaABorrar) {
     remove(archivo.c_str());
     rename("temp.csv", archivo.c_str());
 }
-
+void actualizarCSV(const string& archivo){
+    ifstream input(archivo);
+    ofstream temp("temp.csv");
+    int numero_fila=0;
+    int fila_actualizar, columna_actualizar;
+    string nuevoDato;
+    string linea;
+    int confirmar;
+    cout<< "Ingrese la fila a actualizar: "; cin>>fila_actualizar; fila_actualizar -=1;
+    cout<< "Ingrese el numero de columna a actualizar: "; cin>>columna_actualizar; columna_actualizar= columna_actualizar;
+    cin.ignore(100,'\n');
+    while(getline(input, linea)) {
+        if (numero_fila==fila_actualizar){
+            cout<< "Ingrese el nuevo dato: "; getline(cin,nuevoDato);
+            stringstream ss(linea);
+            string resto_de_fila;
+            int columna_actual= 1;
+            bool columna1= true;
+            while(getline(ss,resto_de_fila,',')){
+                if (!columna1){
+                    temp<<",";
+                }
+                if (columna_actual == columna_actualizar){
+                    temp<< nuevoDato;
+                } else {
+                    temp << resto_de_fila;
+                }
+                columna_actual++;
+                columna1= false;
+            }
+            temp<< endl;
+        } else{
+            temp<< linea << endl;
+        }
+        numero_fila++;
+    }
+    cout<<"Quieres confirmar los cambios? 1. Si | 2. No: "; cin>>confirmar;
+    if(confirmar==1){
+        input.close();
+        temp.close();
+        remove(archivo.c_str());
+        rename("temp.csv", archivo.c_str());
+    } else if(confirmar==2){
+        input.close();
+        temp.close();
+        remove("temp.csv");
+     }
+}
 int main(){
     int opcion1 = 0;
-    cout << "¿Que archivo desea leer? \n 1 = vehiculos | 2 = clientes | 3 = repuestos\n";
+    cout << "Que archivo desea leer? \n 1 = vehiculos | 2 = clientes | 3 = repuestos\n";
     cin >> opcion1;
     ifstream input;
 
     switch (opcion1){
         case 1: { // Vehiculos
             int opcionx = 0;
-            cout << "Que accion desea realizar? \n 1 = Borrar | 2 = Actualizar | 3 = Insercion | 4 = Lectura\n";
+            cout << "Que accion desea realizar? \n 1 = Borrar | 2 = Actualizar | 3 = Insercion | 4 = Lectura \n";
             cin >> opcionx;
             input.open("./bin/Vehiculos.csv");
             if (!input.is_open()) {
@@ -84,9 +131,11 @@ int main(){
                     borrarFilaCSV("./bin/Vehiculos.csv", fila);
                     break;
                 }
-                case 2: // Vehiculos > Actualizar
-                    // falta codigo
+                case 2: {  //Vehiculos Actualizar
+                    input.close();
+                    actualizarCSV("./bin/Vehiculos.csv");
                     break;
+                } 
                 case 3: // Vehiculos > Insercion
                     // falta codigo
                     break;
@@ -117,9 +166,11 @@ int main(){
                     borrarFilaCSV("./bin/Clientes.csv", fila);
                     break;
                 }
-                case 2: // Clientes > Actualizar
-                    // falta codigo
+                case 2 : {  //Clientes > Actualizar
+                    input.close();
+                    actualizarCSV("./bin/Clientes.csv");
                     break;
+                }
                 case 3: // Clientes > Insercion
                     // falta codigo
                     break;
@@ -150,9 +201,11 @@ int main(){
                     borrarFilaCSV("./bin/repuestos.csv", fila);
                     break;
                 }
-                case 2: // Repuestos > Actualizar
-                    // falta codigo
+                case 2 : { //Repuestos > Actualizar
+                    input.close();
+                    actualizarCSV("./bin/repuestos.csv");
                     break;
+                }
                 case 3: // Repuestos > Insercion
                     // falta codigo
                     break;
